4fc5f04d42ee65facb91f23b58af3fa03bae9241
須田彩音 <sudaayane@sudaayanenoMacBook-Air.local>
2023-05-16 00:44:00 +0900

    メモアプリ

---
+++ memo.rb
@@ -0,0 +1,29 @@
+require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+
+memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+
+# if文を使用して続きを作成していきましょう。
+# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
+
+puts "拡張子を除いたファイルを入力してください"
+file_type = gets.chomp
+puts "メモしたい内容を記入してください"
+puts "完了したらCtrl + Dをおします"
+
+if memo_type == 1
+  text_type = gets.chomp
+  data = [text_type]
+  CSV.open(file_type + ".csv",'w') do |csv|
+   csv << data
+  end
+elsif memo_type == 2
+  text_type = gets.chomp
+  data = [text_type]
+  CSV.open(file_type + ".csv",'a') do |csv|
+   csv << data
+  end
+end
+
+    
