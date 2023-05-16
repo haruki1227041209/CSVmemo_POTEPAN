55f25f206e5cc575284722d39d4dae80917861f9
大塚春希 <haruki122704@gmail.com>
2023-05-16 18:36:18 +0900

    getsをstdinに変更

--- memo.rb
+++ memo.rb
@@ -13,13 +13,13 @@ puts "メモしたい内容を記入してください"
 puts "完了したらCtrl + Dをおします"
 
 if memo_type == 1
-  text_type = gets.chomp
+  text_type = $stdin.readlines
   data = [text_type]
   CSV.open(file_type + ".csv",'w') do |csv|
    csv << data
   end
 elsif memo_type == 2
-  text_type = gets.chomp
+  text_type = $stdin.readlines
   data = [text_type]
   CSV.open(file_type + ".csv",'a') do |csv|
    csv << data
